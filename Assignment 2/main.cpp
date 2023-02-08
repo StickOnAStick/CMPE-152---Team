e387b2391cff53f1d81f353d4148bece83376179
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-02-07 20:57:46 -0800

    Added table and basic scanner functionality.
    TODO:
    
        Adjust scanner function to work with std::basic_iostreambuff
        to allow for stdin to be read and parsed.

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main () {
+    std::cout << "Cpp is the language for me" << std::endl;
+    return 0;
+}
---
+++ Assignment 2/scanner.h
@@ -0,0 +1,17 @@
+#include <iostream>
+class Scanner {
+
+    private:
+        struct table {
+
+        };
+    
+    public:
+    
+        void nextToken () {
+            std::cout << "Next token fetched" << std::endl;
+        };
+
+
+
+};
